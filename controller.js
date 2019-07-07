185d4aa19ff1b3bfe62d788dfc98c611c62169b3
Jean-Marc Skopek <jskopek@gmail.com>
2019-07-07 14:21:00 -0700

    Refactoring controller to be class-based

--- controller.js
+++ controller.js
@@ -1,14 +1,55 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const path = '/dev/ttyS3'
-const baudRate = 9600;
-const port = new SerialPort(path, { baudRate: baudRate });
 
-const parser = new Readline();
-port.pipe(parser);
+class LEDController {
+    /* responsible for connecting to the LED controller and sending commands. Commands should be sent to an Arduino
+     * via the serial port */
+    init() {
+        this.opened = false
+        this.port = undefined;
+        this.parser = undefined;
+    }
 
-parser.on('data', line => console.log(`received data > ${line}`))
-port.write('ROBOT POWER ON\n')
+    connect(path, baudRate) {
+        this.path = path;
+        this.baudRate = baudRate;
+
+        this.port = new SerialPort(this.path, { baudRate: this.baudRate });
+        this.parser = new Readline();
+        this.port.pipe(this.parser);
+
+        //this.parser.on('data', line => console.log(`received data > ${line}`))
+        this.port.write('LED CONTROLLER ON\n')
+
+
+        this.port.on('error', function(err) {
+            console.log('Error: ', err.message)
+        })
+        this.port.on('open', function(err) {
+            console.log('Port opened!')
+            this.opened = true;
+        })
+
+    }
+    send(ledArray) {
+        /* send an array of [r,g,b] values to the LEDs */
+        let ledArrayString = ledArray.map(rgbArray => rgbArray.join(',')).join(';');
+
+        let command = `<${ledArrayString}>`
+        
+        if(this.opened) {
+            this.port.write(command, (err) => {
+                if(err) { console.log(`error sending data through port baudRate: ${this.baudRate} command: ${command}`); }
+                console.log(`sent command: ${command}`);
+            });
+        } else {
+            console.log(`not connected to LEDController; simulating command: ${command}`);
+        }
+    }
+}
+
+var controller = new LEDController();
+controller.connect('/dev/ttyS3', 9600)
 
 setInterval(() => {
     let lightSequence = [];
@@ -18,13 +59,7 @@ setInterval(() => {
         let blue = parseInt(255 * Math.random());
         lightSequence.push([red, green, blue]);
     }
-
-    let lightSequenceString = lightSequence.map(rgbArray => rgbArray.join(',')).join(';')
-
-    let command = `<${lightSequenceString}>`
-    port.write(command, (err) => {
-        if(err) { console.log(`error sending data through port baudRate: ${baudRate} command: ${choices}`); }
-        console.log(`sent command: ${command}`);
-    });
+    controller.send(lightSequence);
 }, 1000);
-//> ROBOT ONLINE
+
+
